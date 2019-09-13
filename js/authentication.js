26b73bac02265e72a0251c3ed1ce8ad5a4fc5ca3
jonas <jonas@mirar.solutions>
2019-09-13 20:34:23 -0300

    Login criado

---
+++ js/authentication.js
@@ -0,0 +1,20 @@
+const btnLogar = document.getElementById('btn_acessar');
+var inputCPF = document.getElementById('input_cpf');
+var inputPassword = document.getElementById('input_password');
+const slides = document.getElementById('slides');
+
+btnLogar.addEventListener('click', function () {
+
+    firebase.auth().signInWithEmailAndPassword(inputCPF.value, inputPassword.value).then(function (result) {
+        slides.slideNext();
+
+    }).catch(function (error) {
+
+        switch(error.code) {
+            case 'auth/invalid-email': alert('Email não encontrado');
+            break;
+            case 'auth/wrong-password': alert('Senha inválida');
+        };
+
+    });
+});
