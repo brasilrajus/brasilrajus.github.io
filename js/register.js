a98aa0fb189b26cedf820c53e159bea212631ab6
jonas <jonas@mirar.solutions>
2019-09-14 11:24:14 -0300

    Correçoes responsividade

--- js/register.js
+++ js/register.js
@@ -2,6 +2,7 @@ const paciente = document.getElementById('paciente');
 const profissional = document.getElementById('profissional');
 
 const conselhoRegional = document.getElementById('CR');
+const chooseConselhoRegional = document.getElementById('chooseCR');
 const cartaoSus = document.getElementById('cartaoSus'); 
 
 window.onload = aSerCadastrado('paciente');
@@ -10,6 +11,8 @@ function aSerCadastrado(cadastro){
     if(cadastro === 'paciente') {
         conselhoRegional.classList.add('anm_min_height');
         conselhoRegional.classList.remove('anm_max_height');
+        chooseConselhoRegional.classList.add('anm_min_height');
+        chooseConselhoRegional.classList.remove('anm_max_height');
         cartaoSus.classList.add('anm_max_height');
         cartaoSus.classList.remove('anm_min_height');
         paciente.style.fontWeight = "1000";
@@ -17,6 +20,8 @@ function aSerCadastrado(cadastro){
     } else {
         conselhoRegional.classList.remove('anm_min_height');
         conselhoRegional.classList.add('anm_max_height');
+        chooseConselhoRegional.classList.remove('anm_min_height');
+        chooseConselhoRegional.classList.add('anm_max_height');
         cartaoSus.classList.remove('anm_max_height');
         cartaoSus.classList.add('anm_min_height');
         paciente.style.fontWeight = "normal";
