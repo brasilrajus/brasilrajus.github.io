b976ab419349b59b9d25b07fe7cdf906c356557b
JonasVMagalhaes <jonasevmagalhaes@gmail.com>
2019-09-17 13:22:40 -0300

    novo visual incrementado

---
+++ js/animations-cadastrar.js
@@ -0,0 +1,14 @@
+const pacienteSegment = document.getElementById('segment_paciente');
+const prescritorSegment = document.getElementById('segment_prescritor');
+
+const inputCRM = document.getElementById('input_crm');
+const inputSUS = document.getElementById('input_sus');
+
+pacienteSegment.addEventListener('click', cadastrarPaciente);
+prescritorSegment.addEventListener('click', cadastrarPrescritor);
+
+function cadastrarPaciente() {
+}
+function cadastrarPrescritor() {
+
+}
