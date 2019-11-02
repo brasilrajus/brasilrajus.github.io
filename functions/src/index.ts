c40fa44c4749301eb87180f2900d69fca1165d55
jonasevmagalhaes <jonasevmagalhaes@gmail.com>
2019-11-02 18:18:46 -0300

    Guia de orientaçoes

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -49,9 +49,17 @@ const user = onReq((request, response) => {
 })
 
 
+/*************************************/
+/* Funções funcionando perfeitamente */
+/*********************************** */
+
+const lerJsonObject = onReq((request, response) => {
+    response.status(200).json({ users });
+});
+
 const helloWorld = onReq((request, response) => {
-    response.send("Olá, Mundo!")
-})
+    response.send("Olá, Mundo!");
+});
 
 const retornarDocument = onReq((request, response) => {
     let user = 'SGfIngOvZIhhpsIpHwJEIE1Sm9K2';
@@ -69,15 +77,64 @@ const retornarCollection = onReq((request, response) => {
                 rst.push(doc.data())
             })
             response.send(JSON.parse(JSON.stringify(rst)));
-        })
+        });
 });
 
-//---------------------------------
-export { helloWorld, user, writeToFirestore2, userUnico, retornarDocument, retornarCollection }
+const escutarModificacaoNoFirestore = functions.firestore
+    .document('users/SGfIngOvZIhhpsIpHwJEIE1Sm9K2')
+    .onWrite((change, context) => {
+        db.doc('users/SGfIngOvZIhhpsIpHwJEIE1Sm9K2').update({ teste: 'Carlos, pare de me zoar 222' });
+    });
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+//---------------------------------
+export {
+    helloWorld,
+    user,
+    writeToFirestore2,
+    userUnico,
+    retornarDocument,
+    retornarCollection,
+    escutarModificacaoNoFirestore,
+    lerJsonObject
+}
+/**
+ * TODO: Conectar as duas apis - cors
+ * 
+ * Modelo API padrão: https://firestore.googleapis.com/v1/projects/rajus-c8a95/databases/(default)/documents/users
+ * 
+ * [ ] - Colections 'users' Documents - [get] Todos
+ *      nome: users
+ *      metodo(verbo): get
+ *      url(rota): https://us-central1-rajus-c8a95.cloudfunctions.net/users 
+ *      Parametros: {}
+ *      retorno: json [{},{}]
+ * 
+ * [ ] - Colections 'users' Documents - [get] Usando Uid
+ *      nome: users
+ *      metodo(verbo): get
+ *      url(rota): https://us-central1-rajus-c8a95.cloudfunctions.net/users/Uid
+ *      Parametros: {Uid} 
+ *      retorno: json {uid: 'ad1ad1as1da', nome: 'string', ...}
+ * 
+ * [ ] - Colections 'users' Documents - [post] Usando rawBody parametros
+ *      nome: users
+ *      metodo(verbo): post
+ *      url(rota): https://us-central1-rajus-c8a95.cloudfunctions.net/users 
+ *      Parametros: {nome: 'string', ...}
+ *      retorno: confirmação de recebimento pelo firebase ou erro
+ *      
+ * [ ] - Colections 'users' Documents - [put] Usando Uid + rawBody parametros
+ *      nome: users
+ *      metodo(verbo): put
+ *      url(rota): https://us-central1-rajus-c8a95.cloudfunctions.net/users 
+ *      Paramtros: Uid + {nome: 'string', ...}
+ *      retorno: confirmação de recebimento pelo firebase ou erro
+ * 
+ * [ ] - Colections 'users' Documents - [delete] Usando Uid
+ *      nome: users
+ *      metodo(verbo): delete
+ *      url(rota): https://us-central1-rajus-c8a95.cloudfunctions.net/users/Uid
+ *      Parametros: {Uid} 
+ *      retorno: confirmação de recebimento pelo firebase ou erro
+ */
+
