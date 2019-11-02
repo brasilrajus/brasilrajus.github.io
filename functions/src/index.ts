0575f9a79d5afc9d213cd81c36dc40e2bc908c56
jonasevmagalhaes <jonasevmagalhaes@gmail.com>
2019-11-02 03:09:40 -0300

    Lendo collections

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -6,25 +6,74 @@ admin.initializeApp();
 const onReq = functions.https.onRequest;
 
 const users = [
-    {Uid: 1, nome: 'Jonas', idade: 17},
-    {Uid: 2, nome: 'Carlos', idade: 38},
-    {Uid: 3, nome: 'Adriana', idade: 15},
-    {Uid: 4, nome: 'Juliana', idade: 12}
+    { Uid: 1, nome: 'Jonas', idade: 17 },
+    { Uid: 2, nome: 'Carlos', idade: 38 },
+    { Uid: 3, nome: 'Adriana', idade: 15 },
+    { Uid: 4, nome: 'Juliana', idade: 12 }
 ]
 
-const user = onReq((request, response) => {
-    response.status(200).json({ 
-        users
-     })
+const db = admin.firestore();
+
+/* const writeToFirestore = functions.firestore
+    .document('users/SGfIngOvZIhhpsIpHwJEIE1Sm9K2')
+    .onWrite((change, context) => {
+        db.doc('users/SGfIngOvZIhhpsIpHwJEIE1Sm9K2').set({ teste: 'funcionou' });
+    }); */
+
+const writeToFirestore2 = functions.firestore
+    .document('users/SGfIngOvZIhhpsIpHwJEIE1Sm9K2')
+    .onWrite((change, context) => {
+        db.doc('users/SGfIngOvZIhhpsIpHwJEIE1Sm9K2').update({ teste: 'Carlos, pare de me zoar 222' });
+    });
+
+// -----------
+const userUnico = onReq((request, response) => {
+
+    db.collection('users').doc('SGfIngOvZIhhpsIpHwJEIE1Sm9K2').get()
+        .then(() => {
+            response.status(200).json({
+
+            })
+        });
 })
+
+const user = onReq((request, response) => {
+    response.status(200).json({
+        users
+    })
+
+    db.doc('users/SGfIngOvZIhhpsIpHwJEIE1Sm9K2').update({ consoleLog: 'Log limpo' });
+    //OK request.method
+    //* request.rawBody
+    //request.body.text
+})
+
 
 const helloWorld = onReq((request, response) => {
-    response.send("Hello World");
+    response.send("Olá, Mundo!")
 })
 
-export { helloWorld, user }
+const retornarDocument = onReq((request, response) => {
+    let user = 'SGfIngOvZIhhpsIpHwJEIE1Sm9K2';
+    db.collection('users').doc(user).get()
+        .then((snapshot) => {
+            response.send(JSON.parse(JSON.stringify(snapshot.data())));
+        });
+});
 
+const retornarCollection = onReq((request, response) => {
+    db.collection('users').get()
+        .then(snap => {
+            let rst: any = [];
+            snap.forEach(doc => {
+                rst.push(doc.data())
+            })
+            response.send(JSON.parse(JSON.stringify(rst)));
+        })
+});
 
+//---------------------------------
+export { helloWorld, user, writeToFirestore2, userUnico, retornarDocument, retornarCollection }
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
